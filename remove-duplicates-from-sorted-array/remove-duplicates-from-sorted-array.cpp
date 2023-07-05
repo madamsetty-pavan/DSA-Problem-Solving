bd27cc47b909409c0638b6e7334e349e99bfcff0
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-07-05 20:24:04 +0530

    Time: 14 ms, Memory: 18.5 MB - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -3,8 +3,8 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int left = 0, right = 1, n = nums.size();
         while(right<n) {
-            while(right<n && nums[right] == nums[left]) right++;
-            if(right<n) nums[++left] = nums[right++];
+            if(nums[right] == nums[left]) right++;
+            else nums[++left] = nums[right++];
         }
         return left+1;
     }
