02aecb7e46adc612139e42e0f48cef74e14058bc
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-07-05 12:17:30 +0530

    Time: 315 ms (67.99%), Space: 134.8 MB (39.52%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -4,27 +4,16 @@ public:
         if(nums.size()==0) return {};
         vector<int> ans;
         deque<int> dq;
-        int i=0,n=nums.size();
-        while(i<k)
-        {
-            while(dq.size() && nums[dq.back()]<=nums[i])
-            {
-                dq.pop_back();
-            }
-            dq.push_back(i);
-            i++;
+        int i =0, n= nums.size();
+        while(i<k) {
+            while(dq.size() && nums[dq.back()]<nums[i]) dq.pop_back();
+            dq.push_back(i++);
         }
         ans.push_back(nums[dq.front()]);
-        while(i<n)
-        {
-            while(dq.size() && i-dq.front()+1>k)
-            {
-                dq.pop_front();
-            }
-            while(dq.size() && nums[dq.back()]<=nums[i])
-            {
-                dq.pop_back();
-            }
+        while (i < n) {
+            while(dq.size() && i-dq.front()+1 > k) dq.pop_front();
+            
+            while(dq.size() && nums[dq.back()]<nums[i]) dq.pop_back();
             dq.push_back(i);
             ans.push_back(nums[dq.front()]);
             i++;
