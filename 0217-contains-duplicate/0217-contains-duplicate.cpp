bbc14642a868341fbb3b84f9c26a48eb5954f825
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-07-07 23:47:16 +0530

    Time: 159 ms (43.24%), Space: 69.5 MB (45.7%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> mp;
+        for(int i =0;i<nums.size();i++) {
+            if(mp.find(nums[i])!=mp.end()) return true;
+            mp.insert(nums[i]);
+        }
+        return false;
+    }
+};
