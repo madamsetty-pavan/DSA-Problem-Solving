a57332e8df64d5bb77b7b1f4b1535248b1dad409
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-07-10 11:34:19 +0530

    Update 0111-minimum-depth-of-binary-tree.cpp

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -11,13 +11,36 @@
  */
 class Solution {
 public:
+    // int minDepth(TreeNode* root) {
+    //     //DFS Solution
+    //     if(!root) return 0;
+    //     // only right child tree exists
+    //     if(!root->left) return 1+minDepth(root->right);
+    //     // only left child tree exists
+    //     if(!root->right) return 1+minDepth(root->left);
+    //     // both child exists
+    //     return min(minDepth(root->left),minDepth(root->right))+1;
+    // } 
+
+
     int minDepth(TreeNode* root) {
-        if(!root) return 0;
-        // only right child tree exists
-        if(!root->left) return 1+minDepth(root->right);
-        // only left child tree exists
-        if(!root->right) return 1+minDepth(root->left);
-        // both child exists
-        return min(minDepth(root->left),minDepth(root->right))+1;
+        //BFS Solution
+        if(!root)return 0;
+        queue<TreeNode*> qu;
+        qu.push(root);
+        int depth = 1, n;
+        
+        while(!qu.empty()){
+            n = qu.size();
+            for(int i=0;i<n;i++) {
+                TreeNode* root1 = qu.front();
+                qu.pop();
+                if(!root1->left && !root1->right) return depth;
+                if(root1->left) qu.push(root1->left);
+                if(root1->right) qu.push(root1->right);
+            }
+            depth++;
+        }
+        return -1;
     } 
 };
