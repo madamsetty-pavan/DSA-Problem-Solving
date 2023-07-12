3a33a5263ba9eec5c8bba0dff4c137a1ef9f3c3d
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-07-12 12:16:14 +0530

    Time: 75 ms (77.24%), Space: 13.1 MB (72.62%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,39 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        if(nums.size()<4) return {};
-        vector<vector<int>>ans;
-        long long n=nums.size(),sum=0;
+			vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
-        for(int i=0;i+3<n;)
-        {
-            for(int j=i+1;j+2<n;)
-	{
-		int left=j+1,right=n-1;
-		while(left<right)
-		{
-			sum = (long long) nums[i]+nums[j]+nums[left]+nums[right];
-			if(sum==target)
-			{
-				ans.push_back({nums[i],nums[j],nums[left],nums[right]});
-				left++;
-				while(left<right && nums[left]==nums[left-1]) left++;
-				right--;
-				while(left<right && nums[right]==nums[right+1]) right--;
-			}
-			else if(sum>target)
-			{
-				right--;
-			}
-			else left++;
-}
-j++;
-while(j<n && nums[j]==nums[j-1]) j++;
-}
-i++;
-while(i<n && nums[i]==nums[i-1]) i++;
-        }
-return ans;
+				int n = nums.size(), left, right;
+				long long sum = 0;
+				for(int i=0;i+3<n;) {
+					for(int j=i+1;j+2<n;) {
+						left = j+1;
+						right = n-1;
+						while(left<right) {
+							sum = (long long)nums[left] + nums[right] + nums[i] +nums[j];
+							if(sum == target) {
+								ans.push_back({nums[left],nums[right], nums[i], nums[j]});
+								left++;
+								right--;
+								while(left<right && nums[left]==nums[left-1]) left++;
+								while(left<right && nums[right]==nums[right+1]) right--;
+							} else if(sum < target) left++;
+							else right--;
+						}
+						j++;
+						while(j<n && nums[j]==nums[j-1]) j++;
+					}
+					i++;
+					while(i<n && nums[i]==nums[i-1]) i++;
+				}
+				return ans;
     }
 };
-
