97f52400721dcae2b6d32a4bec12fdb6c142a9b2
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-07-13 00:11:00 +0530

    Time: 138 ms (13.79%), Space: 67.6 MB (99.62%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int sum = 0, ans = INT_MIN, n= nums.size();
+        int n = nums.size(), sum =0, ans = INT_MIN, start = 0, end;
         for(int i=0;i<n;i++) {
             sum += nums[i];
-            if(sum<nums[i]) sum = nums[i];
-            ans = max(sum, ans);
+            if(sum<nums[i])  { sum = nums[i]; start =i;}
+            if(ans<=sum) {
+                ans = sum;
+                end = i;
+            }
         }
+        cout<<start<<" "<<end<<" "<<endl;
         return ans;
     }
+
 };
