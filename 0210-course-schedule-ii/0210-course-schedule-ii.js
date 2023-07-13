2ac95ab767aae7c394561cc5f2f884e207a70d03
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-07-13 08:54:29 +0530

    Time: 79 ms (71.73%), Space: 44.7 MB (98.66%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var findOrder = function(numCourses, prerequisites) {
+    let indegree = Array(numCourses).fill(0), graph = new Map(), ans = [];
+
+    for(let i=0;i<numCourses;i++) {
+        graph[i] = [];
+    }    
+    for(let i=0;i<prerequisites.length;i++) {
+        graph[prerequisites[i][1]].push(prerequisites[i][0]);
+        indegree[prerequisites[i][0]]++;
+    }    
+    for(let i=0;i<numCourses;i++) {
+        let j = 0;
+        while(j<numCourses) {
+            if(indegree[j]==0) {
+                break;
+            }
+            j++;
+        }
+        if(j==numCourses) return [];
+        indegree[j]=-1;
+        ans.push(j);
+        for(let k = 0;k<graph[j].length;k++) {
+            indegree[graph[j][k]]--;
+        }
+    }
+    return ans;
+
+};
