f275876453f4434f52a8afb694bf87ab7d31eeaf
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-07-20 12:34:29 +0530

    Time: 21 ms (22.37%), Space: 17.3 MB (93.31%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -3,13 +3,14 @@ public:
     vector<int> asteroidCollision(vector<int>& nums) {
         vector<int> ans;
         bool pushFlag = 1;
-        int n = nums.size();
+        int n = nums.size(), num1, num2;
         for(int i=0;i<n;i++) {
            if(ans.size() && ans.back()>0 && nums[i]<0) {
                pushFlag = 1;
                while(ans.size() && ans.back()>0 && nums[i]<0) {
-                   if(abs(ans.back()) < abs(nums[i])) {ans.pop_back();}
-                   else if(abs(ans.back()) == abs(nums[i])) {
+                   num1 = abs(ans.back()), num2 = abs(nums[i]);
+                   if(num1 < num2) {ans.pop_back();}
+                   else if(num1 == num2) {
                        ans.pop_back();
                        pushFlag = 0;
                        break;
