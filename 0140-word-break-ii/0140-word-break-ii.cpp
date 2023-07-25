0c3f6f2e3f5cc107c4e51f499a615daa25b7a84d
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-07-25 12:08:46 +0530

    Time: 3 ms (37.25%), Space: 8.2 MB (8.93%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -3,23 +3,24 @@ public:
     vector<string> wordBreak(string s, vector<string>& wordDict) {
         unordered_set<string> words(wordDict.begin(),wordDict.end());
         vector<string>ans;
+        int n = s.size();
         unordered_map<int,vector<string>>mp;
-        recurse(s,words,ans,0,mp);
+        recurse(s,words,ans,0,mp,n);
         return mp[0];
     }
     
-    vector<string> recurse(string s, unordered_set<string>&words, vector<string>&ans, int index, unordered_map<int,vector<string>>&mp ) {
+    vector<string> recurse(string s, unordered_set<string>&words, vector<string>&ans, int index, unordered_map<int,vector<string>>&mp, int n ) {
         if(mp.find(index) != mp.end()) {
             return mp[index];
         }
         string str = "";
-        for(int i=index;i<s.size();i++) {
+        for(int i=index;i<n;i++) {
             str += s[i];
             if (words.find(str) != words.end()) {
-                if(i+1==s.size()) {
+                if(i+1==n) {
                     mp[index].push_back(str);
                 } else {
-                    vector<string> value = recurse(s,words,ans,i+1,mp);
+                    vector<string> value = recurse(s,words,ans,i+1,mp,n);
                 for(auto x:mp[i+1]) {
                     mp[index].push_back(str+" "+x);
                 }
