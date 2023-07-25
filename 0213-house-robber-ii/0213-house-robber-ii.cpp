a13ac34617b9a202495ca58760a812e1c620b794
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-07-26 05:06:30 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (77.20%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if(nums.size()<2) return nums.size()>0?nums[0]:0;
-        return max(findMax(nums,0,nums.size()-2),findMax(nums,1,nums.size()-1));
+        int n = nums.size();
+        if(n<2) return n>0?nums[0]:0;
+        return max(findMax(nums,0,n-2),findMax(nums,1,n-1));
     }
     
     int findMax(vector<int>& nums, int start, int end) {
-        if(start>end) return 0; 
         int prev2 = 0,  prev = nums[start], curr;
         for(int i= start+1;i<=end;i++) {
             curr = max(nums[i]+prev2, prev);
