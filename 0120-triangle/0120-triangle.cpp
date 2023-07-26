18fba30bb36f78dff7ff6a48a351d7378abce12a
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-07-26 15:51:35 +0530

    Time: 7 ms (66.99%), Space: 8.9 MB (67.05%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -16,19 +16,40 @@
 // };
 
 
+// Tabulation
+// class Solution {
+// public:
+//     int minimumTotal(vector<vector<int>>& triangle) {
+//         int m = triangle.size();
+//         vector<vector<int>> dp(m, vector<int> (m,-1));
+//         for(int i=m-1;i>=0;i--) {
+//             for(int j=0;j<=i;j++) {
+//                 if(i == m-1) dp[i][j] = triangle[i][j];
+//                 else {
+//                      dp[i][j] = triangle[i][j] + min(dp[i+1][j],dp[i+1][j+1]);
+//                 }
+//             }
+//         }
+//         return dp[0][0];
+//     }
+// };
+
+// Space optimisation
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int m = triangle.size();
-        vector<vector<int>> dp(m, vector<int> (m,-1));
+        vector<int> prev(m, -1);
         for(int i=m-1;i>=0;i--) {
+            vector<int> temp(m,-1);
             for(int j=0;j<=i;j++) {
-                if(i == m-1) dp[i][j] = triangle[i][j];
+                if(i == m-1) temp[j] = triangle[i][j];
                 else {
-                     dp[i][j] = triangle[i][j] + min(dp[i+1][j],dp[i+1][j+1]);
+                     temp[j] = triangle[i][j] + min(prev[j],prev[j+1]);
                 }
             }
+            prev = temp;
         }
-        return dp[0][0];
+        return prev[0];
     }
 };
