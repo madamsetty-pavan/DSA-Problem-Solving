292f817841632af4745dd1abb07018b7b624b715
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-07-26 20:47:11 +0530

    Time: 448 ms (18.70%), Space: 101.4 MB (67.19%) - LeetHub

---
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minSpeedOnTime(vector<int>& dist, double hour) {
+        int left = 1, right = 1e7, mid;
+        while(left<right) {
+            mid = left + (right-left)/2;
+            if(isFeasible(dist,hour, mid)) right = mid;
+            else left = mid+1;
+        }
+        if(isFeasible(dist,hour,left))
+                return left;  
+        else return -1;
+    }
+    
+    bool isFeasible(vector<int>& dist, double &hour, int mid) {
+        double time = 0;
+        for(int i=0;i<dist.size()-1;i++) {
+            time += (dist[i] + mid - 1) / mid;
+        }
+        time += (double) dist.back()/mid;
+        return (time<=hour);
+    }
+};
