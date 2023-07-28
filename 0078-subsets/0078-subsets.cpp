0bdf8f033c6b25a2cb3fe1ea10ae95db1cfbf9d7
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-07-28 13:09:04 +0530

    Time: 3 ms (60.72%), Space: 7.2 MB (60.17%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -2,16 +2,21 @@ class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
-        int index = 0;
-        vector<int>temp;
-        recurse(nums, ans, temp ,index);
+        vector<int> temp;
+        recurse(nums, ans, temp, 0);
         return ans;
     }
     
-    void recurse(vector<int> &nums, vector<vector<int>>& ans, vector<int>&temp ,int index) {
-        if(index>=nums.size()) {ans.push_back(temp); return;}
+    void recurse(vector<int>& nums, vector<vector<int>>& ans,vector<int> &temp, int index ) {
+        if(index==nums.size()) {
+            ans.push_back(temp);
+            return;
+        }
+        // take
         temp.push_back(nums[index]);
-        recurse(nums,ans,temp,index+1);
+        recurse(nums,ans,temp, index+1);
+        
+        //not take
         temp.pop_back();
         recurse(nums,ans,temp,index+1);
     }
