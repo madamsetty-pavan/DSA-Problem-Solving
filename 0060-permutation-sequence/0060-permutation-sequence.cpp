1a59d3c0076d52e969b79f2ce273268aa2dbe169
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-07-28 12:59:31 +0530

    Time: 3 ms (55.53%), Space: 6.2 MB (14.96%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -3,20 +3,19 @@ public:
     string getPermutation(int n, int k) {
         vector<int> nums;
         int fact = 1;
-        for(int i=1;i<n;i++) 
-        {
+        k = k-1;
+        for(int i=1;i<n;i++) {
             fact *= i;
             nums.push_back(i);
         }
         nums.push_back(n);
-        k = k-1;
         string ans = "";
         while(true) {
-            ans += to_string(nums[k/fact]);
-            nums.erase(nums.begin() + (k/fact));
-            if(nums.size()==0)break;
-            k= k%fact;
-            fact = fact/nums.size();
+            ans += to_string(nums[(k/fact)]);
+            nums.erase(nums.begin()+(k/fact));
+            if(nums.size()==0) break;
+            k = k%fact;
+            fact = fact/nums.size(); 
         }
         return ans;
     }
