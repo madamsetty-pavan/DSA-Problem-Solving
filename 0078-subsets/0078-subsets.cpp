e57b6a8c6aeafa217704d51adb0ec27ba00c1204
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-07-28 13:09:32 +0530

    Time: 4 ms (48.54%), Space: 6.9 MB (91.53%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,23 +1,41 @@
-class Solution {
-public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> temp;
-        recurse(nums, ans, temp, 0);
-        return ans;
-    }
+// Approach 1: Using take/ not take policy
+// class Solution {
+// public:
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         vector<vector<int>> ans;
+//         vector<int> temp;
+//         recurse(nums,ans,temp,0);
+//         return ans;
+//     }
     
-    void recurse(vector<int>& nums, vector<vector<int>>& ans,vector<int> &temp, int index ) {
-        if(index==nums.size()) {
-            ans.push_back(temp);
-            return;
+//     void recurse(vector<int> &nums,vector<vector<int>>&ans, vector<int>&temp, int index) {
+//         if(index==nums.size()) {
+//             ans.push_back(temp);
+//             return;
+//         }
+//         temp.push_back(nums[index]);
+//         recurse(nums,ans,temp,index+1);
+//         temp.pop_back();
+//         recurse(nums,ans,temp,index+1);
+//     }
+// };
+
+// Approach 2: Using choice of numbers for an empty field
+class Solution {
+    public:
+        vector<vector<int>> subsets(vector<int>& nums) {
+            vector<vector<int>> ans;
+            vector<int> temp;
+            recurse(nums,ans,temp,0);
+            return ans;
         }
-        // take
-        temp.push_back(nums[index]);
-        recurse(nums,ans,temp, index+1);
         
-        //not take
-        temp.pop_back();
-        recurse(nums,ans,temp,index+1);
-    }
+        void recurse(vector<int>&nums,vector<vector<int>>&ans, vector<int>&temp, int index) {
+            ans.push_back(temp);
+            for(int i=index;i<nums.size();i++) {
+                temp.push_back(nums[i]);
+                recurse(nums,ans,temp,i+1);
+                temp.pop_back();
+            }
+        }
 };
