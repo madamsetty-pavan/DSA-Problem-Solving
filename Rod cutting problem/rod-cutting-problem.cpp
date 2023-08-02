0a6a4ae20119686022d3c88d87a59bdc13123bba
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-08-02 23:02:51 +0530

    Update rod-cutting-problem.cpp

--- Rod cutting problem/rod-cutting-problem.cpp
+++ Rod cutting problem/rod-cutting-problem.cpp
@@ -1,3 +1,4 @@
+// Approach 1:
 int unboundedKnapsack(int n, int w, vector<int> &profit, vector<int> &weight){
     // Write Your Code Here.
     vector<vector<int>> dp(n,vector<int>(w+1,0));
@@ -23,4 +24,26 @@ int cutRod(vector<int> &price, int n)
 	vector<int> nums;
 	for(int i=1;i<=n;i++) nums.push_back(i);
 	return unboundedKnapsack(n,n,price,nums);
+}
+
+// Approach 2:
+int cutRod(vector<int> &profit, int n)
+{
+	// Write your code here.
+    vector<int> prev(n+1,0);
+    for(int i=0;i<=n;i++) {
+         prev[i] = i * profit[0];
+    }
+    for(int index = 1; index<n; index++) {
+        for(int j =0;j<=n;j++) {
+            int notTake = prev[j];
+            int take = 0;
+            int rodLength = index +1;
+            if(rodLength<=j) {
+                take = profit[index]+prev[j-rodLength];
+            }
+            prev[j] = max(take,notTake);
+        }
+    }
+    return prev[n];
 }
