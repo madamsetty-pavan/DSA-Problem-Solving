9dbcbffd07532a8cf990c7438de4522a98e6be19
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-08-04 07:15:28 +0530

    Time: 3 ms (92.26%), Space: 17 MB (32.22%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -3,19 +3,23 @@ public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
         vector<int> temp;
-        recurse(candidates,ans,temp, target,0,0);
+        recurse(candidates, ans, temp, target, 0 , 0);
         return ans;
     }
     
-    void recurse(vector<int>&candidates, vector<vector<int>>&ans, vector<int>&temp, int target,int sum,int index) {
+    void recurse(vector<int>&candidates ,vector<vector<int>>&ans, vector<int>&temp, int target, int sum, int index) {
         if(sum>=target || index==candidates.size()) {
-            if(sum==target) ans.push_back(temp);
+            if(target==sum) ans.push_back(temp);
             return;
         }
-        //not to choose this index
-        recurse(candidates,ans,temp,target,sum,index+1);
+        
+        // not take
+        recurse(candidates, ans, temp, target, sum, index+1);
+        
+        //take 
         temp.push_back(candidates[index]);
-        recurse(candidates,ans,temp,target,sum+candidates[index],index);
+        recurse(candidates, ans, temp, target, sum+candidates[index], index);
         temp.pop_back();
+        
     }
 };
