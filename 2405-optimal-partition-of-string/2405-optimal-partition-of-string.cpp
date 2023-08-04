de9eaa2d71f8612b0a2f022444dbe38d4c039fa0
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-08-04 14:19:20 +0530

    Time: 19 ms (93.35%), Space: 10.3 MB (67.12%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -1,21 +1,40 @@
 // O(n) - Time complexity
 // O(1) - space complexity
+// class Solution {
+// public:
+//     int partitionString(string s) {
+//         vector<int>count(26,0);
+//         int ans = 1,left=0,right=0;
+//         while(right<s.size()) {
+//             if(count[s[right]-'a']!=0) 
+//             {
+//                 ans++;
+//                 while(left<right){
+//                     count[s[left++]-'a']--;
+//                 }
+//             }
+//             count[s[right]-'a']++;
+//             right++;
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int partitionString(string s) {
-        vector<int>count(26,0);
+        vector<int>lastSeen(26,-1);
         int ans = 1,left=0,right=0;
         while(right<s.size()) {
-            if(count[s[right]-'a']!=0) 
+            if(lastSeen[s[right]-'a']>=left) 
             {
                 ans++;
-                while(left<right){
-                    count[s[left++]-'a']--;
-                }
+                left = right;
             }
-            count[s[right]-'a']++;
+            lastSeen[s[right]-'a'] = right;
             right++;
         }
         return ans;
     }
 };
+
