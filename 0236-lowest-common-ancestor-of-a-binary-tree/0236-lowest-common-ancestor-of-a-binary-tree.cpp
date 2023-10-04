3c59c440fbc2adffcfa02a458c803960be3fcb83
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-10-04 10:38:43 -0400

    Time: 14 ms (45.94%), Space: 14.7 MB (17.15%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -7,28 +7,45 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+// Solution 1: Using path arrays
+
+// class Solution {
+// public:
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+//         vector<TreeNode*> path1, path2;
+//         makePath(root, p, path1);
+//         makePath(root,q,path2);
+//         int i = path1.size()-1, j = path2.size()-1;
+//         while(i>=0 && j>=0 && path1[i]==path2[j]) {
+//             i--;
+//             j--;
+//         }
+//         if(i<0) return path1[0];
+//         else if(j<0) return path2[0];
+//         return path1[i+1];
+//     }
+    
+//     bool makePath(TreeNode* root, TreeNode* p, vector<TreeNode*> &path) {
+//         if(!root) return false;
+//         if(root == p || makePath(root->left,p,path) || makePath(root->right,p,path)) {
+//             path.push_back(root);
+//             return true;
+//         } 
+//         return false;
+//     }
+// };
+
+
+// Solution 2: Using recursion
+
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> path1, path2;
-        makePath(root, p, path1);
-        makePath(root,q,path2);
-        int i = path1.size()-1, j = path2.size()-1;
-        while(i>=0 && j>=0 && path1[i]==path2[j]) {
-            i--;
-            j--;
-        }
-        if(i<0) return path1[0];
-        else if(j<0) return path2[0];
-        return path1[i+1];
-    }
-    
-    bool makePath(TreeNode* root, TreeNode* p, vector<TreeNode*> &path) {
-        if(!root) return false;
-        if(root == p || makePath(root->left,p,path) || makePath(root->right,p,path)) {
-            path.push_back(root);
-            return true;
-        } 
-        return false;
+        if(!root || root == p || root == q ) return root;
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if(left && right) return root;
+        if(left) return left;
+        return right;
     }
 };
