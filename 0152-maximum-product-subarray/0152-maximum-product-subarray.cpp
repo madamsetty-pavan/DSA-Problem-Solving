119eb32e130a4d58bdc1666d82b466423887ebc0
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-10-04 16:23:39 -0400

    Time: 10 ms (15.80%), Space: 14.1 MB (24.06%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,14 +1,28 @@
+// Single time
+// class Solution {
+// public:
+//     int maxProduct(vector<int>& nums) {
+//         int maxP = 1, minP = 1, n = nums.size(), ans = INT_MIN;
+//         for(int i=0;i<n;i++) {
+//             if(nums[i]<0) swap(maxP,minP);
+//             maxP = max(nums[i], maxP * nums[i]);
+//             minP = min(nums[i], minP * nums[i]);
+//             ans = max(ans, maxP);
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int minP = nums[0], maxP = nums[0], ans = nums[0];
-        for(int i=1;i<nums.size();i++) {
-            if(nums[i]<0) {
-                swap(minP,maxP);
-            }
-            minP = min(nums[i], minP*nums[i]);
-            maxP = max(nums[i], maxP* nums[i]);
-            ans = max(maxP,ans);
+        int prefP = 1, sufP = 1, ans = INT_MIN, n= nums.size();
+        for(int i=0;i<n;i++) {
+            prefP *= nums[i];
+            sufP *= nums[n-1-i];
+            ans = max(ans, max(prefP, sufP));
+            if(prefP ==0) prefP = 1;
+            if(sufP == 0) sufP = 1;
         }
         return ans;
     }
