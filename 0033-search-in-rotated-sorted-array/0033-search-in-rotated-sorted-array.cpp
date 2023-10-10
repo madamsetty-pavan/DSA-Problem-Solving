b8b991f25042e91a698f16091399e4c2bf582c24
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-10-09 20:58:13 -0400

    Time: 0 ms (100.00%), Space: 11.5 MB (6.19%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -6,12 +6,12 @@ public:
             mid = left + (right-left)/2;
             if(nums[mid] == target) return mid;
             if(nums[mid]<=nums[right]) {
-                if(target>= nums[mid] && target<=nums[right]) {
+                if(target> nums[mid] && target<=nums[right]) {
                     left = mid +1;
                 } else right = mid -1;
             }
             else{
-                if(target >= nums[left] && target <= nums[mid]) {
+                if(target >= nums[left] && target < nums[mid]) {
                     right = mid -1;
                 } else left = mid+1;
             }
