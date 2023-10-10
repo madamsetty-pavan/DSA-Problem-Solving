bd8891733f9cbd45555867e8c91fe249f4233d9f
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-10-10 08:15:54 -0400

    Time: 2 ms (55.61%), Space: 7.5 MB (38.87%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     int splitArray(vector<int>& nums, int k) {
-        int left = 0, right = 0, mid;
-        for(auto x:nums) {
+        int left = INT_MIN, right = 0, mid;
+        for (auto &x:nums) {
             left = max(left,x);
-            right += x; 
+            right += x;
         }
         while(left<right) {
-            mid = left + (right-left)/2;
-            if(isFeasible(nums, mid, k)) right = mid;
-            else left = mid+1;
+            mid = left + (right-left) /2;
+            if(isFeasible(nums,mid,k)) {
+                right = mid;
+            } else left = mid + 1;
         }
         return left;
     }
-
-    bool isFeasible(vector<int>& nums, int total, int k) {
-        int group = 1, sum =0;
+    
+    bool isFeasible(vector<int>&nums,  int mid, int &k) {
+        int sum = 0, splitsCount = 1;
         for(int i=0;i<nums.size();i++) {
             sum += nums[i];
-            if(sum>total) {
+            if (sum > mid) {
+                splitsCount++;
+                if(splitsCount>k) return false;
                 sum = nums[i];
-                group++;
-                if(group>k) return false;
             }
         }
         return true;
