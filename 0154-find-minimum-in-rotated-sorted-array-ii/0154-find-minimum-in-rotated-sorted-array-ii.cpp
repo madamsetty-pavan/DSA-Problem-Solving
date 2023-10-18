c460858f9bf9d9cc049a4f773c9cf581f7b5ece5
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-10-18 09:48:37 -0400

    Time: 4 ms (68.05%), Space: 12.8 MB (6.07%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
@@ -1,22 +1,43 @@
+// class Solution {
+// public:
+//     int findMin(vector<int>& nums) {
+//         int left = 0, right = nums.size()-1, mid;
+//         while(left<right) {
+//             mid = left + (right-left)/2;
+//             if(nums[mid]==nums[left] && nums[mid]==nums[right]) {
+//                 int i = mid+1 , j = mid-1;
+//                 while(i<nums.size()&&nums[i]==nums[i-1]) i++;
+//                 if(i!=nums.size() && nums[i]<nums[mid]) {
+//                     left = mid;
+//                     continue;
+//                 }
+//                 while(j>=0 && nums[j]==nums[j+1]) j--;
+//                 if(j>=0 && nums[j]<nums[mid]) {right = mid;continue;}
+//             }
+//             if(nums[mid]>nums[right]) left = mid+1;
+//             else right = mid;
+//         }
+//         return nums[left];
+//     }
+// };
+
+
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int left = 0, right = nums.size()-1, mid;
-        while(left<right) {
+        int left = 0, right = nums.size()-1, mid, ans= INT_MAX;
+        while(left<=right) {
+            while(left<right && nums[left] == nums[left+1]) left++;
+            while(left<right && nums[right] == nums[right-1]) right--;
             mid = left + (right-left)/2;
-            if(nums[mid]==nums[left] && nums[mid]==nums[right]) {
-                int i = mid+1 , j = mid-1;
-                while(i<nums.size()&&nums[i]==nums[i-1]) i++;
-                if(i!=nums.size() && nums[i]<nums[mid]) {
-                    left = mid;
-                    continue;
-                }
-                while(j>=0 && nums[j]==nums[j+1]) j--;
-                if(j>=0 && nums[j]<nums[mid]) {right = mid;continue;}
+            if(nums[mid]<=nums[right]) {
+                ans = min(nums[mid],ans);
+                right = mid -1;
+            } else {
+                ans = min(ans,nums[left]);
+                left = mid +1;
             }
-            if(nums[mid]>nums[right]) left = mid+1;
-            else right = mid;
         }
-        return nums[left];
+        return ans;
     }
 };
