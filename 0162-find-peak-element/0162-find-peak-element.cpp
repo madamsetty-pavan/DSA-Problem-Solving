b9fe47164b6db4faff3dd1cb71f7069f710a12aa
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-10-18 10:24:49 -0400

    Time: 6 ms (32.95%), Space: 9.2 MB (18.00%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,12 +1,32 @@
+// class Solution {
+// public:
+//     int findPeakElement(vector<int>& nums) {
+//         int left = 0, right = nums.size()-1, mid;
+//         while(left<right) {
+//             mid = left + (right-left)/2;
+//             if(nums[mid]>nums[mid+1]) 
+//                 right = mid;
+//             else left = mid+1;
+//         }
+//         return left;
+//     }
+// };
+
+
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int left = 0, right = nums.size()-1, mid;
-        while(left<right) {
+        int  n = nums.size();
+        if(n==1) return 0;
+        if(nums[0] > nums[1]) return 0;
+        if(nums[n-1]>nums[n-2]) return n-1;
+        int left = 1, right = n-2, mid;
+        while(left<=right) {
             mid = left + (right-left)/2;
-            if(nums[mid]>nums[mid+1]) 
-                right = mid;
-            else left = mid+1;
+            if(nums[mid]>nums[mid+1] && nums[mid] > nums[mid-1]) {
+                return mid;
+            } else if(nums[mid]>nums[mid+1]) right = mid - 1;
+            else left = mid + 1;
         }
         return left;
     }
