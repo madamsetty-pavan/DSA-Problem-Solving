d94828381c49409db465eceee2e9689f0c53e580
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-10-27 12:54:09 -0400

    Time: 2 ms (65.42%), Space: 9.3 MB (23.32%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,31 +1,16 @@
-// class Solution {
-// public:
-//     int findPeakElement(vector<int>& nums) {
-//         int left = 0, right = nums.size()-1, mid;
-//         while(left<right) {
-//             mid = left + (right-left)/2;
-//             if(nums[mid]>nums[mid+1]) 
-//                 right = mid;
-//             else left = mid+1;
-//         }
-//         return left;
-//     }
-// };
-
-
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int  n = nums.size();
-        if(n==1) return 0;
-        if(nums[0] > nums[1]) return 0;
-        if(nums[n-1]>nums[n-2]) return n-1;
-        int left = 1, right = n-2, mid;
+        int left = 0, right = nums.size()-1, mid;
+        if(left == right) return left; // only one element
+        if(nums[left] > nums[left+1]) return left;
+        if(nums[right] > nums[right-1]) return right;
+        left++;
+        right--;
         while(left<=right) {
             mid = left + (right-left)/2;
-            if(nums[mid]>nums[mid+1] && nums[mid] > nums[mid-1]) {
-                return mid;
-            } else if(nums[mid]>nums[mid+1]) right = mid - 1;
+            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) return mid;
+            else if(nums[mid] > nums[mid+1]) right = mid - 1 ;
             else left = mid + 1;
         }
         return left;
