781eb122a0059a34a8dac99431ffe39a5efb1981
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-10-27 13:06:42 -0400

    Time: 37 ms (56.88%), Space: 19.4 MB (28.36%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int left = 1, right = INT_MIN, mid;
-        for(auto &x:piles) {
-            right = max(right, x);
+        for(auto &x: piles) {
+            right = max(right,x);
         }
         while(left<right) {
             mid = left + (right-left)/2;
             if(isFeasible(piles, mid, h)) right = mid;
-            else left = mid + 1;
+            else left = mid+1;
         }
         return left;
     }
     
-    bool isFeasible(vector<int>&piles, int speed, int h) {
+    bool isFeasible(vector<int>& piles, int mid, int h) {
         int totalHours = 0;
         for(auto &x:piles) {
-            totalHours += ceil((double)x/speed);
+            totalHours += ceil((double)x/mid);
         }
         return totalHours<=h;
     }
