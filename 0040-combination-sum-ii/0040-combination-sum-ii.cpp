14a2532408c4ee0f5bb490d43d7193baabcd1f14
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-11-02 13:22:55 -0400

    Time: 5 ms (49.68%), Space: 10.9 MB (81.03%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
-        vector<vector<int>>ans;
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> ans;
         vector<int> temp;
-        recurse(candidates,temp,ans,target,0,0);
+        recurse(candidates, ans, temp, target, 0);
         return ans;
     }
-    
-    void recurse(vector<int>& candidates,vector<int> &temp, vector<vector<int>>&ans,int target, int index,int sum) {    
-        if(sum>=target) {
-            if(sum == target) ans.push_back(temp);
+
+    void recurse(vector<int>&candidates, vector<vector<int>>&ans, vector<int>&temp, int target,int index){
+        if(target<=0) {
+            if(target==0) ans.push_back(temp);
             return;
         }
-        for(int i = index; i<candidates.size(); i++) {
-            if(i!=index && candidates[i]==candidates[i-1]) continue;
+        for(int i = index;i<candidates.size();i++) {
+            if(i!=index && candidates[i]==candidates[i-1] ) continue;
             temp.push_back(candidates[i]);
-            recurse(candidates, temp, ans, target, i+1, sum+candidates[i]);
+            recurse(candidates, ans, temp, target - candidates[i], i+1);
             temp.pop_back();
         }
     }
