d61481f22e5a3f035c63a9775f2ef65b542cf2d1
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-11-18 08:24:01 -0500

    Time: 7 ms (93.06%), Space: 20.1 MB (60.47%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,21 +1,41 @@
+// class Solution {
+// public:
+//     int trap(vector<int>& height) {
+//         int maxHeight = 0, maxIndex = 0;
+//         for(int i=0;i<height.size();i++) {
+//             if(maxHeight < height[i]) {
+//                 maxHeight = height[i];
+//                 maxIndex = i;
+//             }
+//         }
+//         int leftMax = 0 , rightMax = 0, ans = 0;
+//         for(int i=0;i<maxIndex;i++) {
+//             leftMax = max(leftMax, height[i]);
+//             ans += min(leftMax, maxHeight) - height[i];
+//         }
+//         for(int i = height.size()-1; i>maxIndex; i--) {
+//             rightMax = max(rightMax, height[i]);
+//             ans += min(rightMax, maxHeight) - height[i];
+//         }
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int maxHeight = 0, maxIndex = 0;
-        for(int i=0;i<height.size();i++) {
-            if(maxHeight < height[i]) {
-                maxHeight = height[i];
-                maxIndex = i;
+        int leftMax = 0, rightMax = 0, left = 0, right = height.size()-1, ans = 0;
+        while(left < right) {
+            if(height[left] <= height[right]) {
+                leftMax = max(height[left], leftMax);
+                ans += leftMax - height[left];
+                left++;
+            }  else {
+                rightMax = max(height[right], rightMax);
+                ans += rightMax - height[right];
+                right--;
             }
-        }
-        int leftMax = 0 , rightMax = 0, ans = 0;
-        for(int i=0;i<maxIndex;i++) {
-            leftMax = max(leftMax, height[i]);
-            ans += min(leftMax, maxHeight) - height[i];
-        }
-        for(int i = height.size()-1; i>maxIndex; i--) {
-            rightMax = max(rightMax, height[i]);
-            ans += min(rightMax, maxHeight) - height[i];
         }
         return ans;
     }
