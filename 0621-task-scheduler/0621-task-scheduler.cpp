94e37a5d878f4cf4d548cc973dc1c1341292322b
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-11-25 18:04:42 -0500

    Time: 90 ms (34.91%), Space: 44.4 MB (20.58%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -0,0 +1,72 @@
+// My own solution which is not working
+// class Solution {
+// public:
+//     struct Compare {
+//         bool operator()(const pair<int, int>& x, const pair<int, int>& y) const {
+//             if (x.second == y.second)
+//                 return x.first < y.first;
+//             return x.second > y.second;
+//         }
+//     };   
+//     int leastInterval(vector<char>& tasks, int n) {
+//         priority_queue<pair<int, int>, vector<pair<int, int>>, Compare> pq;
+//         unordered_map<char,int> mp;
+//         for(auto &x: tasks) {
+//             mp[x]++;
+//         }
+//         for(auto x=mp.begin();x!=mp.end();x++) {
+//             pq.push({x->second, 0});
+//         }
+//         int ans = 0, count = 0;
+//         while (pq.size()) {
+//             ans++;
+//             auto x = pq.top();
+//             cout<<x.first<< " "<<x.second<<" "<<ans<<endl;
+//             if(x.second > ans) {
+//                 cout<<"inside "<<x.first<< " "<<x.second<<endl;
+//                 continue;
+//             }
+            
+//             x.second = ans+n+1;
+//             x.first--;
+//             pq.pop();
+//             if(x.first > 0) pq.push(x);
+//         }
+//         return ans;
+//     }
+// };
+
+class Solution {
+    public: 
+        int leastInterval(vector<char>&tasks, int n) {
+            unordered_map<char, int> mp;
+            priority_queue<int> pq;
+            for (auto &x:tasks) {
+                mp[x]++;
+            }
+            for (auto &x: mp) {
+                pq.push(x.second);
+            }
+            int ans = 0;
+            while(pq.size()) {
+                vector<int> temp;
+                int cycle = n+1;
+                while(cycle>0 && pq.size()) {
+                    auto x = pq.top();
+                    pq.pop();
+                    if(x>1) {
+                        temp.push_back(--x);
+                    }
+                    ans++;
+                    cycle--;
+                }
+                
+                for(auto &x:temp) {
+                    pq.push(x);
+                }
+                if(pq.size()==0) break;
+                ans += cycle;
+            }
+            return ans;
+        }
+};
