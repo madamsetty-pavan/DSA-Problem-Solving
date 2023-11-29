ca0977bd4b7fb50fac7f8d3b680e32333fc7349d
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-11-29 01:17:23 -0500

    Time: 24 ms (54.37%), Space: 23.6 MB (76.10%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -7,16 +7,34 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+// class Solution {
+// public:
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+//         if(!root) return root;
+//         if(!root || root== p || root== q) return root;
+//         TreeNode* left = lowestCommonAncestor(root->left, p, q);
+//         TreeNode* right = lowestCommonAncestor(root->right, p, q);
+//         if(left && right) return root;
+//         if(left) return left;
+//         return right;
+//     }
+//  };
+
 
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(!root) return root;
-        if(!root || root== p || root== q) return root;
-        TreeNode* left = lowestCommonAncestor(root->left, p, q);
-        TreeNode* right = lowestCommonAncestor(root->right, p, q);
-        if(left && right) return root;
-        if(left) return left;
-        return right;
+        int large = max(p->val, q->val);
+        int small = min(p->val, q->val);
+        return checkLCA(root, large, small);
+    }
+    
+    TreeNode* checkLCA(TreeNode* &root, int &large, int &small) {
+        if(root->val> large) {
+            return checkLCA(root->left, large,small);
+        } else if(root->val < small) {
+            return checkLCA(root->right, large, small);
+        }
+        return root;
     }
  };
