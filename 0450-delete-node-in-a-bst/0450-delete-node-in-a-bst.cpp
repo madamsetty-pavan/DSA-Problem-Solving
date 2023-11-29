5c3ecae919fbaa894c73e15ac329dab8ceb4f933
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-11-29 00:57:50 -0500

    Time: 30 ms (19.89%), Space: 33 MB (34.14%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -13,21 +13,32 @@ class Solution {
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
         if(!root) return root;
-        if(root->val > key) {
-            root->left = deleteNode(root->left, key);
-        } else if(root->val < key) {
-            root->right =  deleteNode(root->right, key);
-        } else {
-            if(!root->left && !root->right) return NULL;
-            if(root->left && !root->right)  return root->left;
-            if(root->right && !root->left) return root->right;
-            TreeNode* temp = root->right;
-            while(temp->left) {
-            temp = temp->left;
+        if(root->val == key) {
+            if(!root->left && !root->right) {
+                return NULL;
+            } else if(!root->right) {
+                return root->left;
+            } else {
+                TreeNode* temp = root->right, *prev = root;
+                while(temp->left) {
+                    prev = temp;
+                    temp = temp->left;
+                }
+                if(temp != root->right) {
+                    if(temp->right) prev->left = temp->right;
+                    else prev -> left = NULL;
+                    temp -> left = root->left;
+                    temp -> right = root->right;
+                    return temp;
+                } else {
+                    temp->left = root->left;
+                    return temp;
+                }
             }
-            temp -> left = root->left;
-            return root->right;
         }
+        if(root->val > key) root->left = deleteNode(root->left, key);
+        else if(root->val < key) root->right = deleteNode(root->right, key);
         return root;
     }
+    
 };
