b46e863edb4c3006340ffc8afbde7648f840e7bd
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-12-08 11:39:59 -0500

    Update 1463-cherry-pickup-ii.cpp

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -3,26 +3,26 @@
 // class Solution {
 // public:
     
-//     long long recurse(vector<vector<int>>& grid, int i, int j1, int j2)  {
-//         if(j1<0 || j1>=grid[0].size() || j2<0 || j2>=grid[0].size()) return -1e8;
-//         if(i==grid.size()-1) {
-//             if(j1==j2) return grid[i][j1];
-//             else return grid[i][j1]+grid[i][j2];
-//         }
-//         long long ans = 0;
-//         for(int l1=-1;l1<2;l1++)  {
-//             for(int l2=-1;l2<2;l2++) {
-//                 if(j1 == j2) ans = max(ans, grid[i][j1]+recurse(grid,i+1,j1+l1,j2+l2));
-//                 else ans = max(ans, grid[i][j1]+grid[i][j2]+recurse(grid,i+1,j1+l1,j2+l2));
-//             }
-//         }
-//         return ans;
-//     }
+    long long recurse(vector<vector<int>>& grid, int i, int j1, int j2)  {
+        if(j1<0 || j1>=grid[0].size() || j2<0 || j2>=grid[0].size()) return -1e8;
+        if(i==grid.size()-1) {
+            if(j1==j2) return grid[i][j1];
+            else return grid[i][j1]+grid[i][j2];
+        }
+        long long ans = 0;
+        for(int l1=-1;l1<2;l1++)  {
+            for(int l2=-1;l2<2;l2++) {
+                if(j1 == j2) ans = max(ans, grid[i][j1]+recurse(grid,i+1,j1+l1,j2+l2));
+                else ans = max(ans, grid[i][j1]+grid[i][j2]+recurse(grid,i+1,j1+l1,j2+l2));
+            }
+        }
+        return ans;
+    }
     
-//     int cherryPickup(vector<vector<int>>& grid) {
-//         return recurse(grid,0,0,grid[0].size()-1);
-//     }
-// };
+    int cherryPickup(vector<vector<int>>& grid) {
+        return recurse(grid,0,0,grid[0].size()-1);
+    }
+};
 
 // Recurse + Memo
 class Solution {
@@ -50,3 +50,79 @@ public:
         return recurse(grid,0,0,grid[0].size()-1,dp);
     }
 };
+
+
+#include <bits/stdc++.h> 
+
+// // Bottom up
+int maximumChocolates(int r, int c, vector<vector<int>> &grid) {
+    // Write your code here.
+    vector<vector<vector<int>>> dp(r, vector<vector<int>>(c,vector<int>(c,0)));
+
+    for(int j=0;j<c;j++) {
+        for(int j1=c-1;j1>=0;j1--) {
+            if(j==j1) dp[r-1][j][j1] = grid[r-1][j];
+            else dp[r-1][j][j1] = grid[r-1][j]+grid[r-1][j1];
+        }
+    }
+
+    for(int i=r-2;i>=0;i--) {
+        for(int a=0;a<c;a++) {
+            for(int b=c-1;b>=0;b--) {
+                if(a==b) {
+                    dp[i][a][b] = grid[i][a];
+                } else 
+                    dp[i][a][b] = grid[i][a] + grid[i][b];
+                int temp1 = 0;
+                for(int i1 = -1;i1<2;i1++) {
+                    for(int j1=-1;j1<2;j1++) {
+                        if(a+i1>=0 && a+i1<c && b+j1>=0 && b+j1<c) {
+                             temp1 = max(temp1, dp[i+1][a+i1][b+j1]);
+                        }
+                    }
+                }
+                dp[i][a][b] +=temp1;
+            }
+        }
+    }
+
+    return dp[0][0][c-1];
+}
+
+
+// Bottom-up - Space Optimised
+int maximumChocolates(int r, int c, vector<vector<int>> &grid) {
+    // Write your code here.
+    vector<vector<vector<int>>> dp(2, vector<vector<int>>(c,vector<int>(c,0)));
+
+    for(int j=0;j<c;j++) {
+        for(int j1=c-1;j1>=0;j1--) {
+            if(j==j1) dp[1][j][j1] = grid[r-1][j];
+            else dp[1][j][j1] = grid[r-1][j]+grid[r-1][j1];
+        }
+    }
+
+    for(int i=r-2;i>=0;i--) {
+        for(int a=0;a<c;a++) {
+            for(int b=c-1;b>=0;b--) {
+                if(a==b) {
+                    dp[0][a][b] = grid[i][a];
+                } else 
+                    dp[0][a][b] = grid[i][a] + grid[i][b];
+                int temp1 = 0;
+                for(int i1 = -1;i1<2;i1++) {
+                    for(int j1=-1;j1<2;j1++) {
+                        if(a+i1>=0 && a+i1<c && b+j1>=0 && b+j1<c) {
+                             temp1 = max(temp1, dp[1][a+i1][b+j1]);
+                        }
+                    }
+                }
+                dp[0][a][b] +=temp1;
+            }
+        }
+        dp[1] = dp[0];
+    }
+
+    return dp[0][0][c-1];
+}
+
