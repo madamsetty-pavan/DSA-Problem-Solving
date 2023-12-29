730365fe3920b6440f665ec78dc496c02b3ea908
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2023-12-29 10:27:21 -0500

    Time: 342 ms (23.90%), Space: 10.2 MB (74.63%) - LeetHub

--- 1947-maximum-compatibility-score-sum/1947-maximum-compatibility-score-sum.cpp
+++ 1947-maximum-compatibility-score-sum/1947-maximum-compatibility-score-sum.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     int maxCompatibilitySum(vector<vector<int>>& students, vector<vector<int>>& mentors) {
-        int ans = 0;
+        int ans = 0, n = students.size(), m = mentors.size();
         vector<bool> mentorIsTaken(students.size(),0);
-        recurse(students, mentors, 0, mentorIsTaken, ans, 0);
+        recurse(students, mentors, 0, mentorIsTaken, ans, 0, n, m);
         return ans;
     }
     
-    void recurse(vector<vector<int>>& students, vector<vector<int>>& mentors, int studentIndex, vector<bool>&mentorIsTaken, int &ans, int score) {
-        if(studentIndex == students.size()) {
+    void recurse(vector<vector<int>>& students, vector<vector<int>>& mentors, int studentIndex, vector<bool>&mentorIsTaken, int &ans, int score, int &n, int &m) {
+        if (studentIndex == n) {
             ans = max(ans, score);
             return;
         }
-        for(int i=0;i<mentors.size();i++) {
+        for(int i=0;i<m;i++) {
             if(!mentorIsTaken[i]) {
                 int tempScore = checkCommonScores(students[studentIndex], mentors[i]);
                 mentorIsTaken[i] = true;
-                recurse(students, mentors, studentIndex+1,  mentorIsTaken, ans, score + tempScore);
+                recurse(students, mentors, studentIndex+1,  mentorIsTaken, ans, score + tempScore, n,m);
                 mentorIsTaken[i] = false;
             }
         }
     }
     
     int checkCommonScores(vector<int>& studentScore, vector<int>& mentorScore) {
-        int ans = 0;
-        for(int i=0;i<studentScore.size();i++) {
+        int ans = 0, n = studentScore.size();
+        for(int i=0;i<n;i++) {
             ans += (studentScore[i] == mentorScore[i]);
         }
         return ans;
