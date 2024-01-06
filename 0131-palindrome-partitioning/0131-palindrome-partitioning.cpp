e4165aacf3852c00429114e48936c8838e93b14c
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2024-01-05 19:32:50 -0500

    Time: 87 ms (78.63%), Space: 49.8 MB (81.46%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -2,31 +2,30 @@ class Solution {
 public:
     vector<vector<string>> partition(string s) {
         vector<vector<string>> ans;
-        vector<string> temp;
-        int n = s.size();
-        recursion(0, s, ans, temp, n);
+        vector<string> tempAns;
+        recurse(0, s, tempAns, ans);
         return ans;
     }
     
-    void recursion(int index, string &s , vector<vector<string>>&ans, vector<string>&temp, int &n) {
-        if(index==n) {
-            ans.push_back(temp);
+    void recurse(int index, string &s, vector<string>&tempAns,vector<vector<string>> &ans) {
+        if(index == s.size()) {
+            ans.push_back(tempAns);
             return;
         }
-        for(int i=index;i<n;i++) {
-            if(isPalindrome(s, index, i)) {
-                temp.push_back(s.substr(index, i-index+1));
-                recursion(i+1, s, ans, temp, n);
-                temp.pop_back();
+        string str = "";
+        for(int i=index;i<s.size();i++) {
+            str += s[i];
+            if(isPalindrome(str)) {
+                tempAns.push_back(str);
+                recurse(i+1, s, tempAns, ans);
+                tempAns.pop_back();
             }
         }
     }
     
-    bool isPalindrome(string &s, int i, int j) {
-        while(i<=j) {
-            if(s[i]!=s[j]) return false;
-            i++;
-            j--;
+    bool isPalindrome(string &str) {
+        for(int i=0, j= str.size()-1; i<j;i++,j--) {
+            if(str[i]!=str[j]) return false;
         }
         return true;
     }
