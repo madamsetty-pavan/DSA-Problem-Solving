e155bd99653cfc6e7497164538655d9045aae9c9
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2024-01-10 03:28:54 -0500

    Time: 49 ms (20.13%), Space: 24.8 MB (18.27%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -12,33 +12,32 @@ public:
                 }
             }
         }
-        vector<int> indexes1, indexes2;
+        string ans = "";
         int i = n, j = m;
-        while(indexes1.size()<dp[n][m]) {
-            if(str1[i-1] == str2[j-1]) {
-                indexes1.push_back(i-1);
-                indexes2.push_back(j-1);
+        while(i>0 && j>0)
+        {
+            if(str1[i-1]==str2[j-1])
+            {
+                ans+=str1[i-1];
                 i--;
                 j--;
-            } else {
-                if(dp[i-1][j] >= dp[i][j-1]) i--;
-                else j--;
+            }
+            else
+            {
+                if(dp[i-1][j]>dp[i][j-1])
+                {
+                    ans+=str1[i-1];
+                    i--;
+                }
+                else
+                {
+                    ans+=str2[j-1];
+                    j--;
+                }
             }
         }
-        string ans = "";
-        i = n-1, j = m-1;
-        for(int count=0;count<indexes1.size();count++) {
-            while(i>indexes1[count]) {
-                ans += str1[i--];
-            }
-            while(j>indexes2[count]) {
-                ans += str2[j--];
-            }
-            ans+=str1[i--];
-            j--;
-        }
-        while(i>=0) ans += str1[i--];
-        while(j>=0) ans += str2[j--];
+        while(i>0) { ans += str1[i-1]; i--;}
+        while(j>0) {ans += str2[j-1]; j--;}
         reverse(ans.begin(), ans.end());
         return ans;
     }
