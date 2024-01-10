27ca0caa9d61b0116a1bbfed5f9cef3e4c3ababc
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2024-01-10 03:08:15 -0500

    Time: 48 ms (21.48%), Space: 24.9 MB (9.25%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -12,20 +12,12 @@ public:
                 }
             }
         }
-        // for(int i=1;i<=n;i++) {
-        //     for(int j=1;j<=m;j++) {
-        //         cout<<dp[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
         vector<int> indexes1, indexes2;
         int i = n, j = m;
         while(indexes1.size()<dp[n][m]) {
-            // cout<<i<<" "<<j<<endl;
             if(str1[i-1] == str2[j-1]) {
                 indexes1.push_back(i-1);
                 indexes2.push_back(j-1);
-                // cout<<"indexes "<<indexes1.back()<<" "<<indexes2.back()<<endl;
                 i--;
                 j--;
             } else {
@@ -33,13 +25,9 @@ public:
                 else j--;
             }
         }
-        // for(int i=0;i<indexes1.size();i++) {
-        //     cout<<indexes1[i] << " "<<indexes2[i]<<endl;
-        // }
         string ans = "";
         i = n-1, j = m-1;
         for(int count=0;count<indexes1.size();count++) {
-            // cout<<indexes1[count]<<" "<<indexes2[count]<<endl;
             while(i>indexes1[count]) {
                 ans += str1[i--];
             }
@@ -48,9 +36,7 @@ public:
             }
             ans+=str1[i--];
             j--;
-            // cout<<"ans "<<ans<<endl;
         }
-        // cout<<ans<<endl;
         while(i>=0) ans += str1[i--];
         while(j>=0) ans += str2[j--];
         reverse(ans.begin(), ans.end());
