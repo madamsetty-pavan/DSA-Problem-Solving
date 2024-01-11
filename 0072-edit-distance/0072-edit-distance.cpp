45c8cc2386337b30776b9a969dceb32dfdfef9d4
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2024-01-11 11:16:05 -0500

    Time: 15 ms (43.91%), Space: 9.3 MB (69.07%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -16,29 +16,97 @@
 
 
 // Top Down with memo
+// class Solution {
+// public:
+//     int minDistance(string word1, string word2) {
+//         int n = word1.size(), m = word2.size();
+//         vector<vector<int>> dp(word1.size(), vector<int>(word2.size(),-1));
+//         return recurse(word1.size()-1, word2.size()-1, word1, word2,dp);
+//     }
+//     int recurse(int index1, int index2, string &word1, string &word2, vector<vector<int>> &dp) {
+//         if(index1<0) return index2+1;
+//         if(index2<0) return index1+1;
+//         if(dp[index1][index2] !=-1) return dp[index1][index2];
+//         if(word1[index1] == word2[index2]) {
+//             return dp[index1][index2]=recurse(index1-1, index2-1, word1, word2, dp);
+//         }
+//         int ans = INT_MAX;
+//         // Insert a new character at given index
+//         ans = min(ans, 1+recurse(index1, index2-1, word1, word2, dp));
+//         // Replace the given character
+//         ans = min(ans, 1+recurse(index1-1, index2-1, word1, word2,dp));
+//         // Delete the given character
+//         ans = min(ans, 1+recurse(index1-1, index2, word1, word2, dp));
+//         return dp[index1][index2]=ans;
+//     }
+// };
+
+
+
+
+// Bottom up
 class Solution {
 public:
     int minDistance(string word1, string word2) {
         int n = word1.size(), m = word2.size();
-        if(n==0) return m;
-        if(m==0) return n;
-        vector<vector<int>> dp(word1.size(), vector<int>(word2.size(),-1));
-        return recurse(word1.size()-1, word2.size()-1, word1, word2,dp);
-    }
-    int recurse(int index1, int index2, string &word1, string &word2, vector<vector<int>> &dp) {
-        if(index1<0) return index2+1;
-        if(index2<0) return index1+1;
-        if(dp[index1][index2] !=-1) return dp[index1][index2];
-        if(word1[index1] == word2[index2]) {
-            return dp[index1][index2]=recurse(index1-1, index2-1, word1, word2, dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1,0));
+        for(int i=0;i<=n;i++) {
+            dp[i][0] = i;
         }
-        int ans = INT_MAX;
-        // Insert a new character at given index
-        ans = min(ans, 1+recurse(index1, index2-1, word1, word2, dp));
-        // Replace the given character
-        ans = min(ans, 1+recurse(index1-1, index2-1, word1, word2,dp));
-        // Delete the given character
-        ans = min(ans, 1+recurse(index1-1, index2, word1, word2, dp));
-        return dp[index1][index2]=ans;
+        for(int j=0;j<=m;j++) {
+            dp[0][j] = j;
+        }
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=m;j++) {
+                if(word1[i-1] == word2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    // Insert a new character
+                    dp[i][j] = 1 + dp[i][j-1];
+                    // Replace a given character
+                    dp[i][j] = min(dp[i][j] , 1 + dp[i-1][j-1]);
+                    // Delete a given character
+                    dp[i][j] = min(dp[i][j], 1 + dp[i-1][j]);
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int minDistance(string word1, string word2) {
+//         int n = word1.size(), m = word2.size();
+//         vector<vector<int>> dp(n+1, vector<int>(m+1,0));
+//         for(int i=0;i<=n;i++) {
+//             dp[i][0] = i;
+//             dp[0][i] = i;
+//         }
+//         for(int i=1;i<n;i++) {
+//             for(int j=1;j<m;j++) {
+//                 if(word1[i-1] == word2[j-1]) {
+//                     dp[i][j] = dp[i-1][j-1];
+//                 } else {
+//                     // Insert a new character
+//                     dp[i][j] = 1 + dp[i][j-1];
+//                     // Replace a given character
+//                     dp[i][j] = min(dp[i][j] , 1 + dp[i-1][j-1]);
+//                     // Delete a given character
+//                     dp[i][j] = min(dp[i][j], 1 + dp[i-1][j]);
+//                 }
+//             }
+//         }
+//         return dp[n][m];
+//     }
+// };
+
+
+
+
+
+
+
+
