bca7d992042d88b6f3d71ad5c986b7b66eb03df4
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2024-01-11 15:25:30 -0500

    Time: 61 ms (50.03%), Space: 28.4 MB (10.00%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,3 +1,4 @@
+// NO copy, PAVAN wrote it
 // Top Down 
 // class Solution {
 // public:
@@ -45,41 +46,65 @@
 
 
 // TOP Down - DP
+// class Solution {
+// public:
+//     bool isMatch(string s, string p) {
+//         int n = s.size(), m = p.size();
+//         vector<vector<int>> dp(n, vector<int>(m,-1));
+//         return recurse(s,p,s.size()-1,p.size()-1, dp);
+//     }
+    
+//     bool recurse(string &s, string &p, int i, int j, vector<vector<int>> &dp) {
+//         if(i<0) {
+//             if(j<0) return true;
+//             while(j>=0 && p[j]=='*') j--;
+//             return j<0;
+//         }
+//         if(j<0) {
+//             return false;
+//         }
+//         if(dp[i][j]!=-1) return dp[i][j];
+//         if(s[i]==p[j]) {
+//             return dp[i][j]=recurse(s,p,i-1,j-1,dp);
+//         }
+//         else if(p[j]=='?') {
+//             return dp[i][j]=recurse(s,p,i-1,j-1,dp);
+//         }
+//         else if(p[j] == '*') {
+//             return dp[i][j]=(
+//                 // No match for *
+//                 recurse(s,p,i,j-1,dp) ||
+//                 // Match for 1
+//                 recurse(s,p,i-1,j-1,dp) ||
+//                 // More match
+//                 recurse(s,p,i-1,j,dp)
+//             );
+//         }
+//          return dp[i][j]=false;
+//     }
+    
+// };
+
+// Bottom up
 class Solution {
 public:
     bool isMatch(string s, string p) {
         int n = s.size(), m = p.size();
-        vector<vector<int>> dp(n, vector<int>(m,-1));
-        return recurse(s,p,s.size()-1,p.size()-1, dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1,0));
+        // Base conditions
+        dp[0][0] = 1;
+        for(int j=1;j<=m;j++) {
+            if(p[j-1] == '*' && dp[0][j-1] == 1)
+                dp[0][j] = 1;
+        }
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=m;j++) {
+                if(s[i-1]==p[j-1] || p[j-1]=='?') dp[i][j]= dp[i-1][j-1];
+                else if(p[j-1] == '*') {
+                    dp[i][j]= (dp[i][j-1] || dp[i-1][j-1] || dp[i-1][j]);
+                }
+            }
+        }
+        return dp[n][m];
     }
-    
-    bool recurse(string &s, string &p, int i, int j, vector<vector<int>> &dp) {
-        if(i<0) {
-            if(j<0) return true;
-            while(j>=0 && p[j]=='*') j--;
-            return j<0;
-        }
-        if(j<0) {
-            return false;
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==p[j]) {
-            return dp[i][j]=recurse(s,p,i-1,j-1,dp);
-        }
-        else if(p[j]=='?') {
-            return dp[i][j]=recurse(s,p,i-1,j-1,dp);
-        }
-        else if(p[j] == '*') {
-            return dp[i][j]=(
-                // No match for *
-                recurse(s,p,i,j-1,dp) ||
-                // Match for 1
-                recurse(s,p,i-1,j-1,dp) ||
-                // More match
-                recurse(s,p,i-1,j,dp)
-            );
-        }
-         return false;
-    }
-    
 };
