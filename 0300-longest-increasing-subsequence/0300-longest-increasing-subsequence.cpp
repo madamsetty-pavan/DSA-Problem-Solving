e91595c974e9ba71dac80e59a0cb06cd010b5f09
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2024-01-13 19:19:29 -0500

    Time: 1228 ms (5.03%), Space: 292.3 MB (16.03%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -18,25 +18,45 @@
 //     }
 // };
 
-// Tp Down DP
+// Top Down DP
+// class Solution {
+// public:
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<vector<int>> dp(n,vector<int>(n+1,-1));
+//         return recurse(nums, n-1, n, dp, n);
+//     }
+    
+//     int recurse(vector<int>&nums, int index, int prev, vector<vector<int>> &dp, int &n) {
+//         if(index==0) {
+//             return dp[index][prev] = (prev == n || nums[index] < nums[prev]);
+//         }
+//         if(dp[index][prev] != -1) return dp[index][prev];
+//         int notTake = recurse(nums, index-1, prev, dp,n);
+//         int take = 0;
+//         if(prev == n || nums[index]<nums[prev]) {
+//             take = 1+recurse(nums, index-1, index, dp,n);
+//         }
+//         return dp[index][prev] = max(take, notTake);
+//     }
+// };
+
+// Bottom up DP
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n,vector<int>(n+1,-1));
-        return recurse(nums, n-1, n, dp, n);
-    }
-    
-    int recurse(vector<int>&nums, int index, int prev, vector<vector<int>> &dp, int &n) {
-        if(index==0) {
-            return dp[index][prev] = (prev == n || nums[index] < nums[prev]);
+        vector<vector<int>> dp(n,vector<int>(n+1,0));
+        for(int i = 0;i<n;i++) {
+            for(int prev=n;prev>=0;prev--) {
+                if(i==0) {
+                    if(prev == n || nums[i]<nums[prev]) dp[i][prev] = 1;
+                } else {
+                    dp[i][prev] = dp[i-1][prev];
+                    if(prev == n || nums[i]<nums[prev]) dp[i][prev] = max(dp[i][prev], 1 + dp[i-1][i]);
+                }
+            }
         }
-        if(dp[index][prev] != -1) return dp[index][prev];
-        int notTake = recurse(nums, index-1, prev, dp,n);
-        int take = 0;
-        if(prev == n || nums[index]<nums[prev]) {
-            take = 1+recurse(nums, index-1, index, dp,n);
-        }
-        return dp[index][prev] = max(take, notTake);
+        return dp[n-1][n];
     }
 };
