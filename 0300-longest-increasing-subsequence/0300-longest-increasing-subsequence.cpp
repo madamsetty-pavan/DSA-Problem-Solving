893e25b5bba82863b0ae855e2aca85e9adb11104
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2024-01-13 22:33:16 -0500

    Time: 260 ms (28.96%), Space: 11 MB (28.77%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -41,22 +41,70 @@
 //     }
 // };
 
-// Bottom up DP
+//Bottom up DP
+// class Solution {
+// public:
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<vector<int>> dp(n,vector<int>(n+1,0));
+//         for(int i = 0;i<n;i++) {
+//             for(int prev=n;prev>=0;prev--) {
+//                 if(i==0) {
+//                     if(prev == n || nums[i]<nums[prev]) dp[i][prev] = 1;
+//                 } else {
+//                     dp[i][prev] = dp[i-1][prev];
+//                     if(prev == n || nums[i]<nums[prev]) dp[i][prev] = max(dp[i][prev], 1 + dp[i-1][i]);
+//                 }
+//             }
+//         }
+//         for(int i=0;i<n;i++) {
+//             for(int prev = 0;prev<=n;prev++) {
+//                 cout<<dp[i][prev]<<" ";
+//             }
+//             cout<<endl;
+//         }
+        
+//         return dp[n-1][n];
+//     }
+// };
+
+
+// Bottom up DP - Space optmised
+// class Solution {
+// public:
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<vector<int>> dp(2,vector<int>(n+1,0));
+//         for(int i = 0;i<n;i++) {
+//             for(int prev=n;prev>=0;prev--) {
+//                 if(i==0) {
+//                     if(prev == n || nums[i]<nums[prev]) dp[1][prev] = 1;
+//                 } else {
+//                     dp[1][prev] = dp[0][prev];
+//                     if(prev == n || nums[i]<nums[prev]) dp[1][prev] = max(dp[1][prev], 1 + dp[0][i]);
+//                 }
+//             }
+//             dp[0] = dp[1];
+//         }
+//         return dp[1][n];
+//     }
+// };
+
+
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n,vector<int>(n+1,0));
-        for(int i = 0;i<n;i++) {
-            for(int prev=n;prev>=0;prev--) {
-                if(i==0) {
-                    if(prev == n || nums[i]<nums[prev]) dp[i][prev] = 1;
-                } else {
-                    dp[i][prev] = dp[i-1][prev];
-                    if(prev == n || nums[i]<nums[prev]) dp[i][prev] = max(dp[i][prev], 1 + dp[i-1][i]);
+        vector<int> dp(n, 1);
+        int ans = 0;
+        for(int i=0;i<n;i++) {
+            for(int prev = 0;prev<i;prev++) {
+                if(nums[i]>nums[prev]) {
+                    dp[i] = max(dp[i], 1 + dp[prev]);
                 }
             }
+            ans  = max(ans, dp[i]);
         }
-        return dp[n-1][n];
+        return ans;
     }
 };
