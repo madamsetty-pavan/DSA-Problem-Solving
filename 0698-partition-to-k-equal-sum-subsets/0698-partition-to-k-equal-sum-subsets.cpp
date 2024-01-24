0a785412b318bf66d0b324f4db02712bcf58e77c
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2024-01-23 22:43:37 -0500

    Time: 262 ms (42.24%), Space: 10.9 MB (49.52%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -1,33 +1,43 @@
 class Solution {
 public:
-    bool canPartitionKSubsets(vector<int>& nums, int k) {
+   bool canPartitionKSubsets(vector<int>& nums, int K) {
+        int N = nums.size();
+        if (K == 1) return true;
+        if (N < K) return false;
         int sum = 0;
-        for(auto &x:nums) {
-            sum += x;
-        }
-        if(sum % k != 0 || k>sum) return false;
-        sum = sum/k;
-        for(auto &x:nums) {
-            if(x>sum) return false;
-        }
-        int n = nums.size();
-        vector<bool> visited(n,0);
-        return recurse(nums, 0, 0, sum, 0, n, k, visited);
+        for (int i = 0; i < N; i++) sum += nums[i];
+        if (sum % K != 0) return false;
+
+        int subset = sum / K;
+        int subsetSum[K];
+        bool taken[N];
+
+        for (int i = 0; i < K; i++) subsetSum[i] = 0;
+        for (int i = 0; i < N; i++) taken[i] = false;
+
+        subsetSum[0] = nums[N - 1];
+        taken[N - 1] = true;
+
+        return canPartitionKSubsets(nums, subsetSum, taken, subset, K, N, 0, N - 1);
     }
-    
-    bool recurse(vector<int>&nums, int currSet, int currSum, int &requiredSum, int index, int &n, int &k, vector<bool>&visited) {
-        if(currSum == requiredSum) {
-            if(currSet == k-1) return true;
-            return recurse(nums, currSet+1, 0, requiredSum, 0, n, k, visited);
+
+    bool canPartitionKSubsets(vector<int>& nums, int subsetSum[], bool taken[], int subset, int K, int N, int curIdx, int limitIdx) {
+        if (subsetSum[curIdx] == subset) {
+            if (curIdx == K - 2) return true;
+            return canPartitionKSubsets(nums, subsetSum, taken, subset, K, N, curIdx + 1, N - 1);
         }
-        for(int i=index;i<n;i++) {
-            int sum = currSum + nums[i] ;
-            if(visited[i] == false && (sum <= requiredSum)) {
-                visited[i] = true;
-                if(recurse(nums, currSet, sum, requiredSum, i+1, n, k, visited)) {
-                    return true;
-                }
-                visited[i] = false;
+
+        for (int i = limitIdx; i >= 0; i--) {
+            if (taken[i]) continue;
+            int tmp = subsetSum[curIdx] + nums[i];
+
+            if (tmp <= subset) {
+                taken[i] = true;
+                subsetSum[curIdx] += nums[i];
+                bool nxt = canPartitionKSubsets(nums, subsetSum, taken, subset, K, N, curIdx, i - 1);
+                taken[i] = false;
+                subsetSum[curIdx] -= nums[i];
+                if (nxt) return true;
             }
         }
         return false;
