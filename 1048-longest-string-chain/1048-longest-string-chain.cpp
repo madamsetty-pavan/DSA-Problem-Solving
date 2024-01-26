58907ce9c9c69706935b7ecfd28e9e3a5d6bdcd6
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2024-01-25 20:02:09 -0500

    Time: 52 ms (91.48%), Space: 16.8 MB (63.08%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int longestStrChain(vector<string>& words) {
+        int n = words.size(),ans = 1;
+        vector<int> dp(n,1);
+        sort(words.begin(), words.end(), compare);
+        for(int i=0;i<n;i++) {
+            for(int prev=0;prev<i;prev++) {
+                if(checkIfIS(words[prev], words[i])) {
+                    if(dp[i] < 1 + dp[prev]) {
+                        dp[i] = 1 + dp[prev];
+                    }
+                } 
+            }
+            ans = max(ans, dp[i]);
+        }
+        return ans;
+    }
+    
+    bool static compare(string &s1, string &s2) {
+        return s1.size()<s2.size();
+    }
+    
+    bool checkIfIS(string &s1, string &s2) {
+        int i = 0, j = 0, n = s1.size(), m = s2.size(), flag = 0;
+        if(n==m || m-n>1) return false;
+        while(i<n && j<m) {
+            if(s1[i] != s2[j]) {
+                if(flag == 1) return false;
+                flag = 1;
+                j++;
+                continue;
+            }
+            i++;
+            j++;
+        }
+        return (j>=m-1);
+    }
+};
