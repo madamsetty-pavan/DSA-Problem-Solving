cb163d573f857c15897cfcdef08dfd714c1fe9d8
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2024-01-26 11:28:20 -0500

    Time: 26 ms (96.80%), Space: 15.7 MB (8.30%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-     int findNumberOfLIS(vector<int>& nums) {
-        int n = nums.size(), res = 0, max_len = 0;
-        vector<pair<int,int>> dp(n,{1,1});            //dp[i]: {length, number of LIS which ends with nums[i]}
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j <i ; j++){
-                if(nums[i] > nums[j]){
-                    if(dp[i].first == dp[j].first + 1)dp[i].second += dp[j].second;
-                    if(dp[i].first < dp[j].first + 1)dp[i] = {dp[j].first + 1, dp[j].second};
+    int findNumberOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<pair<int, int>> dp(n,{1,1});
+        int ans = 0;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<i;j++) {
+                if(nums[i] > nums[j]) {
+                    if(dp[i].first < dp[j].first + 1) {
+                        dp[i] = {dp[j].first + 1 , dp[j].second};
+                    } else if(dp[i].first == dp[j].first +1) {
+                        dp[i].second += dp[j].second; 
+                    }
                 }
             }
-            if(max_len == dp[i].first)res += dp[i].second;
-            if(max_len < dp[i].first){
-                max_len = dp[i].first;
-                res = dp[i].second;
-            }
+            ans = max(ans, dp[i].first);
         }
-        return res;
+        int count = 0;
+        for(int i=0;i<n;i++) {
+            if(dp[i].first == ans) count += dp[i].second;
+        }
+        return count;
     }
 };
