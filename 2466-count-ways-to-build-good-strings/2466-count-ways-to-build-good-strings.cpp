c7db28a0f658cb7d5a308076599d4f3de709ce64
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2024-01-30 03:19:21 -0500

    Time: 7 ms (80.81%), Space: 19.8 MB (19.68%) - LeetHub

---
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -0,0 +1,43 @@
+// class Solution {
+// public:
+//     int countGoodStrings(int low, int high, int zero, int one) {
+//         string zString = "";
+//         string oString = "";
+//         for(int i=0;i<zero;i++) {
+//             zString += '0';
+//         }
+//         for(int i=0;i<one;i++) {
+//             oString += '1';
+//         }
+//         vector<int> dp(high+1,-1);
+//         return (recurse("", low, high, zString, oString, dp));
+//     }
+//     int recurse(string str, int &low, int &high, string &zString ,string &oString, vector<int>&dp) {
+//         int n = str.size();
+//         if(n>high) return 0;
+//         int ans = 0;
+//         if(dp[n]!=-1) return dp[n];
+//         if(n>=low && n<=high) ans += 1;
+//         ans += recurse(str + zString, low, high, zString, oString,dp);
+//         ans += recurse(str + oString, low, high, zString, oString,dp);
+//         return dp[n]=ans% 1000000007;
+//     }
+// };
+
+
+class Solution {
+public:
+    int countGoodStrings(int low, int high, int zero, int one) {
+        vector<int> dp(high+1,-1);
+        return (recurse(0, low, high, zero, one, dp));
+    }
+    int recurse(int n, int &low, int &high,int zero, int one, vector<int>&dp) {
+        if(n>high) return 0;
+        int ans = 0;
+        if(dp[n]!=-1) return dp[n];
+        if(n>=low && n<=high) ans += 1;
+        ans += recurse(n+zero, low, high, zero, one,dp);
+        ans += recurse(n+one, low, high, zero, one,dp);
+        return dp[n]=ans% 1000000007;
+    }
+};
