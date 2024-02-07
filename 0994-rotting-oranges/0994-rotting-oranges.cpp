09b7eb50dcad4743ec2a1d1a04b416586b2b0f63
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2024-02-07 00:17:53 -0500

    Time: 10 ms (25.77%), Space: 15.9 MB (21.75%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,38 +3,38 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
         queue<pair<int,int>> qu;
-        vector<vector<int>> directions{{-1,0},{1,0},{0,1},{0,-1}};
+        vector<int> directions = {-1,0,1,0,-1};
+        int fresh = 0;
         for(int i=0;i<n;i++) {
             for(int j=0;j<m;j++) {
                 if(grid[i][j] == 2) {
                     qu.push({i,j});
                 }
+                if(grid[i][j] == 1) {
+                    fresh++;
+                }
             }
         }       
-        int ans = 0;
+        int ans = -1;
         while(!qu.empty()) {
             ans++;
-            int n1 = qu.size();
-            for(int i=0;i<n1;i++) {
+            int sz = qu.size();
+            while(sz--) {
                 auto front = qu.front();
                 qu.pop();
-                //cout<<front.first<<" "<<front.second<<" ";
                 for(int j=0;j<4;j++) {
-                    int x = front.first + directions[j][0];
-                    int y = front.second + directions[j][1];
+                    int x = front.first + directions[j];
+                    int y = front.second + directions[j+1];
                     if(x<n && y<m && x>=0 && y>= 0 && grid[x][y] == 1) {
                         grid[x][y] = 2;
+                        fresh--;
                         qu.push({x,y});
                     }
                 }
             }
-            //cout<<endl;
         }
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<m;j++) {
-                if(grid[i][j] == 1) return -1;
-            }
-        }
-        return ans>0?ans-1:0;
+        if(fresh>0) return -1;
+        if(ans == -1) return 0;
+        return ans;
     }
 };
