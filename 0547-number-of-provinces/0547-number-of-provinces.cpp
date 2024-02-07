e2a46b8a93d4c70d56ab4017b509a20aa4457d02
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2024-02-07 01:34:28 -0500

    Time: 18 ms (87.74%), Space: 18.1 MB (26.20%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -14,10 +14,9 @@ public:
     }
     
     void recurse(vector<vector<int>>& isConnected, vector<int>&visited, int index) {
-        if(visited[index]) return;
-        visited[index] = 1;
         for(int i=0;i<isConnected[index].size();i++) {
             if(isConnected[index][i] == 1 && visited[i] == 0) {
+                visited[i] = 1;
                 recurse(isConnected, visited,i);   
             }
         }
