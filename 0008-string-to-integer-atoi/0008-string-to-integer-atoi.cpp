29e325420b31a35f8729854594ce59df93a57e77
PAVAN MADAMSETTY <madamsetty.pavan@gmail.com>
2024-03-08 21:00:41 -0500

    Time: 0 ms (100.00%), Space: 8.3 MB (30.51%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,23 +1,43 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int n = s.size(),i = 0 ;
-        while(i<n && s[i] == ' ') {
-                i++;
-        }
-        int sign = 1;
-        if(s[i]=='-' || s[i]=='+') {
-            if(s[i]=='-') sign = -1;
-            i++;
-        }
-        int ans = 0;
-        while(i<n && s[i]>='0' && s[i]<='9') {
-            if(ans>INT_MAX/10 || (ans==INT_MAX/10 && s[i]>'7')){
-                if(sign == 1) return INT_MAX;
-                else return INT_MIN;
+        int n = s.size();
+        int index = 0, sign = 1, notInt = 0;
+        while(index<n) {
+            if(s[index] =='-') {
+                if(notInt) return 0;
+                sign = -1;
+                notInt = 1;
+                index++;
+            } else if(s[index] == ' ') {
+                if(notInt) { return 0;}
+                index++;
+            } else if(s[index]>='0' && s[index]<='9'){
+                break;
+            } else if(s[index]=='+'){
+                if(notInt) return 0;
+                notInt = 1;
+                index++;
+            } else {
+                return 0;
             }
-            ans = ans *10 +(s[i++]-'0');
         }
-        return ans*sign;
+        int val = recurse(s, index, 0, n, sign);
+        if(val == INT_MIN) return val;
+        return sign * val;
+    }
+    
+    int recurse(string&s, int index, long long number, int &stringSize, int &sign) {
+        if(index == stringSize) return number;
+        if(s[index] == ' ') return number;
+        if(!(s[index]>='0' && s[index]<='9')) return number;
+        // sign check
+        if(sign == 1) {
+            if(number>INT_MAX/10 || (number == INT_MAX/10 && s[index]>'7')) return INT_MAX;
+        } else if(sign == -1) {
+            if(number>INT_MAX/10 || (number == INT_MAX/10 && s[index]>'8')) return INT_MIN;
+        }
+        number = number *10 + (s[index]-'0');
+        return recurse(s, index+1, number, stringSize, sign);
     }
 };
